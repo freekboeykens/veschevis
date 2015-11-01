133080cc3406fd4afd654e6f5675d7a2c364a751
freekboeykens <freek.boeykens@gmail.com>
2015-11-01 12:28:08 +0100

    Add cooperant model

--- vesche_vis/subscriptions/models.py
+++ vesche_vis/subscriptions/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Cooperant(models.Model):
+    class Meta:
+        verbose_name: "Coöperant"
+        verbose_name_plural: "Coöperanten"
+
+    first_name = models.CharField('Voornaam', max_length=100)
+    last_name = models.CharField('Achternaam', max_length=100)
+    phone = models.CharField('Telefoon', max_length=100)
+    email = models.EmailField('E-mail', max_length=100)
+    code = models.CharField('Code', max_length=100)
+    street = models.CharField('Straat', max_length=100)
+    number = models.PositiveIntegerField('Nummer')
+    mailbox = models.CharField('Bus', max_length=5)
+    zip_code = models.PositiveIntegerField('Postcode')
+    city = models.CharField('Stad', max_length=100)
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
