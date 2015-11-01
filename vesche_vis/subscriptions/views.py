4fc711d25907b6c342aa64259835fa9ff92ad634
freekboeykens <freek.boeykens@gmail.com>
2015-11-01 13:39:17 +0100

    Add simple create view form to add cooperants

--- vesche_vis/subscriptions/views.py
+++ vesche_vis/subscriptions/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
 
-# Create your views here.
+class CooperantCreateView(CreateView):
+    model = Cooperant
+    fields = [
+        'first_name',
+        'last_name',
+        'phone',
+        'email',
+        'street',
+        'number',
+        'mailbox',
+        'zip_code',
+        'city',
+    ]
+
+    # TODO: use stand-alone function to create unique code?
