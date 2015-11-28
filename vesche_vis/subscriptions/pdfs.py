66fa6d3504afbb73ffdbe58b8ffd9a3cf7e29815
freekboeykens <freek.boeykens@gmail.com>
2015-11-28 23:06:38 +0100

    create signature list for collection point

--- vesche_vis/subscriptions/pdfs.py
+++ vesche_vis/subscriptions/pdfs.py
@@ -19,12 +19,36 @@ class CollectionPointPDF:
         buffer = BytesIO()
 
         # Create the PDF object, using the BytesIO object as its "file."
-        p = canvas.Canvas(buffer, pagesize=A4)
+        p = canvas.Canvas(buffer, pagesize = A4)
+        width, self.height = A4
+        styles = getSampleStyleSheet()
 
-        # Draw things on the PDF. Here's where the PDF generation happens.
-        # See the ReportLab documentation for the full list of functionality.
+        # WRite collection point name
+        collectionpoint = """ <font size="9">
+        Afhaalpunt: %s<br/>
+        </font>
+        """ % (self.collection_point.name)
+        par = Paragraph(collectionpoint, styles["Normal"])
+        par.wrapOn(p, width, self.height)
+        par.drawOn(p, *self.coord(18, 40, mm))
 
-        p.drawString(100, 100, self.collection_point.name)
+        # write data
+        data = []
+        data.append(["Coöperant", "Pakket", "Aantal", "Handtekening"])
+        for subscription in self.collection_point.subscription_set.all():
+            row = []
+            row.append(subscription.cooperant)
+            row.append(subscription.subscription_type)
+            row.append(subscription.amount)
+            row.append("")
+            data.append(row)
+        t = Table(data, 1.5 * inch)
+        t.setStyle(TableStyle([
+            ('INNERGRID', (0,0), (-1,-1), 0.25, colors.black),
+            ('BOX', (0,0), (-1,-1), 0.25, colors.black)
+        ]))
+        t.wrapOn(p, width, self.height)
+        t.drawOn(p, *self.coord(18, 85, mm))
 
         # Close the PDF object cleanly.
         p.showPage()
@@ -35,3 +59,11 @@ class CollectionPointPDF:
         buffer.close()
 
         return pdf
+
+    def coord(self, x, y, unit=1):
+        """
+        # http://stackoverflow.com/questions/4726011/wrap-text-in-a-table-reportlab
+        Helper class to help position flowables in Canvas objects
+        """
+        x, y = x * unit, self.height -  y * unit
+        return x, y
